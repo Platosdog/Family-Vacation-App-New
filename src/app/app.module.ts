5b4f7f0090b4104827fb67e545ce4d176374e10f
Platosdog <62915572+Platosdog@users.noreply.github.com>
2023-10-26 10:34:48 +0900

    ng fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatNativeDateModule} from '@angular/material/core';
 import { PlannerComponent } from './planner/planner.component';
 import {MatSelectModule} from '@angular/material/select';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -27,7 +28,7 @@ import {MatSelectModule} from '@angular/material/select';
     AppRoutingModule,
     BrowserAnimationsModule, MatToolbarModule, MatButtonModule, MatIconModule,
     MatMenuModule, MatDatepickerModule, MatFormFieldModule, MatInputModule,
-    MatDatepickerModule, MatNativeDateModule, MatSelectModule
+    MatDatepickerModule, MatNativeDateModule, MatSelectModule, FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
