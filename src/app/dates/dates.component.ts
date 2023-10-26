5e86c037f3857264d2f750e6b78816bf7acd9233
Platosdog <62915572+Platosdog@users.noreply.github.com>
2023-10-26 10:03:05 +0900

    ng difficulty

--- src/app/dates/dates.component.ts
+++ src/app/dates/dates.component.ts
@@ -1,16 +1,23 @@
-import { Component } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-dates',
   templateUrl: './dates.component.html',
   styleUrls: ['./dates.component.css'],
 })
-export class DatesComponent {
-  date = new FormControl(new Date());
+export class DatesComponent implements OnInit {
+  days = [1, 2, 3, 4, 5, 6, 7];
+  selectedDays = 1;
 
-  exportDate() {
-     const selectedDate = this.date.value;
-     console.log('Selected Date:', selectedDate);
+  @Output() selectedDaysChange: EventEmitter<number> = new EventEmitter<number>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+     this.selectedDaysChange.emit(this.selectedDays);
+  }
+
+  onChange() {
+     this.selectedDaysChange.emit(this.selectedDays);
   }
  }
