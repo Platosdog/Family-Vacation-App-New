6e3ccd70b999564aad94cbd14211e257f65eec4a
Platosdog <62915572+Platosdog@users.noreply.github.com>
2024-02-16 10:46:20 +0900

    added routerlink

--- src/app/plan/plan.component.ts
+++ src/app/plan/plan.component.ts
@@ -2,22 +2,13 @@ import {Component} from '@angular/core';
 
 export interface PeriodicElement {
   name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+  day: number;
+  meal: string;
+  other: string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  {day: 1, name: 'J1K1', meal: 'lunch', other: 'cesear chicken wraps'},
 ];
 
 @Component({
@@ -26,6 +17,6 @@ const ELEMENT_DATA: PeriodicElement[] = [
   styleUrls: ['./plan.component.css'],
 })
 export class PlanComponent {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  displayedColumns: string[] = ['day', 'name', 'meal', 'other'];
   dataSource = ELEMENT_DATA;
 }
